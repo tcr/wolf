b7891d62dbdcf503a99ccade2f3aab9a1291b288
Tim Cameron Ryan <id@timryan.org>
2013-01-03 18:06:48 -0500

    Bump to Rem v0.6

--- wolf.js
+++ wolf.js
@@ -4,9 +4,10 @@ var colors = require('colors');
 
 // Define Wolfram Alpha's manifest.
 var manifest = {
-  "id": "wolfram-alpha",
+  "id": "wolframalpha.com",
   "name": "Wolfram Alpha",
   "control": "https://developer.wolframalpha.com/portal/apisignup.html",
+  "configuration": ["key"],
 
   "base": "http://api.wolframalpha.com/v2",
   "configParams": {
@@ -22,39 +23,45 @@ var manifest = {
 };
 
 // Create the Wolfram Alpha API.
-var wolf = rem.create(manifest, {
+rem.createClient(manifest).configure({
   format: 'xml'
-}).prompt();
+}).promptConfiguration(function (err, wolf) {
 
-function query(input) {
-  // Make REST query.
-  wolf('query').get({input: input}, function (err, xml, media) {
-    if (xml.get('//error')) {
-      // Error message.
-      console.error('Error:', xml.get('//error/msg').text());
-      process.exit(Number(xml.get('//error/code').text()));
-    } else {
-      // Print plaintext pods.
-      xml.find('//pod').forEach(function (pod) {
-        console.log((pod.attr('title').value() || '').yellow);
-        pod.find('subpod').forEach(function (subpod) {
-          if (subpod.attr('title').value()) {
-            console.log(' ' + (subpod.attr('title').value() || '').bold.red);
-          }
-          subpod.find('plaintext').forEach(function (plaintext) {
-            if (plaintext.text().trim()) {
-              console.log(prefixLines('   ', plaintext.text()));
+  function query (input) {
+    // Make the REST query.
+    wolf('query').get({input: input}, function (err, xml, media) {
+      if (xml.get('//error')) {
+        // Error message.
+        console.error('Error:', xml.get('//error/msg').text());
+        process.exit(Number(xml.get('//error/code').text()));
+      } else {
+        // Print plaintext pods.
+        xml.find('//pod').forEach(function (pod) {
+          console.log((pod.attr('title').value() || '').yellow);
+          pod.find('subpod').forEach(function (subpod) {
+            if (subpod.attr('title').value()) {
+              console.log(' ' + (subpod.attr('title').value() || '').bold.red);
             }
-          });
+            subpod.find('plaintext').forEach(function (plaintext) {
+              if (plaintext.text().trim()) {
+                console.log(prefixLines('   ', plaintext.text()));
+              }
+            });
+          })
         })
-      })
-    }
-  });
-}
+      }
+    });
+  }
 
-function prefixLines(prefix, text) {
-  return prefix + text.trim().split(/\s*\n\s*/).join('\n' + prefix);
-}
+  function prefixLines(prefix, text) {
+    return prefix + text.trim().split(/\s*\n\s*/).join('\n' + prefix);
+  }
 
-// Command line.
-query(process.argv.slice(2).join(' '));
+  // Command line.
+  var input = process.argv.slice(2).join(' ');
+  if (!input) {
+    console.error('Usage: wolf [query]');
+    process.exit(1);
+  }
+  query(input);
+});
